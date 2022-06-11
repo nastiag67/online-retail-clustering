088fa79be58f5d37e6294cd658f654eb04a33c89
nastiag67 <anastasia.glushkova0@yandex.ru>
2022-06-11 09:41:47 +0300

    profiling depends on clustering

--- tools/preprocessing/eda.py
+++ tools/preprocessing/eda.py
@@ -34,7 +34,7 @@ class Dataset:
     def get_transformed(self):
         # 1. Correct data types
         self.df = self._correct_types()
-        # 2. Feature Engineering
+        # 2. Add engineered features
         self.df = self._add_features()
         return self.df
 
@@ -42,13 +42,34 @@ class Dataset:
         self.df['CustomerID'] = self.df['CustomerID'].astype('Int64')
         return self.df
 
+    def _add_features(self):
+        self.df['InvoiceYear'] = pd.to_datetime(self.df['InvoiceDate'], errors='coerce').dt.year
+        self.df['InvoiceMonth'] = pd.to_datetime(self.df['InvoiceDate'], errors='coerce').dt.month
+        self.df['InvoiceDay'] = pd.to_datetime(self.df['InvoiceDate'], errors='coerce').dt.day
+        self.df.drop('InvoiceDate', inplace=True, axis=1)
+        self.features.remove('InvoiceDate')
+        self.features.extend(['InvoiceYear', 'InvoiceMonth', 'InvoiceDay'])
+
+        self.df['Revenue'] = self.df['UnitPrice'] * self.df['Quantity']
+        return self.df
+
     def _get_features(self):
         assert set(self.features_ohe).issubset(set(self.features)), \
             "`features_ohe` to be OHE must be a part of `features`."
         # subset of df with only necessary features
-        self.df = self.df[self.features]
-        self.df = self._ohe(self.features_ohe)
-        return self.df
+        df = self.df[self.features]
+        df = self._ohe(self.features_ohe)
+        return df
+
+    def get_clustering_df(self):
+
+        # self.get_profiling_df()
+
+        # 3. One Hot Encoding
+        if self.features_ohe is not None:
+            df = self._get_features()
+
+        return df
 
     def _ohe(self, cols):
         """Performs One Hot Encoding for features in `cols`.
@@ -73,53 +94,35 @@ class Dataset:
                 self.df.drop(columns=colname, inplace=True)
         return self.df
 
-    def _add_features(self):
-        self.df['InvoiceYear'] = pd.to_datetime(self.df['InvoiceDate'], errors='coerce').dt.year
-        self.df['InvoiceMonth'] = pd.to_datetime(self.df['InvoiceDate'], errors='coerce').dt.month
-        self.df['InvoiceDay'] = pd.to_datetime(self.df['InvoiceDate'], errors='coerce').dt.day
-        self.df.drop('InvoiceDate', inplace=True, axis=1)
-        self.features.remove('InvoiceDate')
-        self.features.extend(['InvoiceYear', 'InvoiceMonth', 'InvoiceDay'])
-
-        self.df['Revenue'] = self.df['UnitPrice'] * self.df['Quantity']
-        return self.df
-
-    def get_clustering_df(self):
-        self.df = self.get_profiling_df()
-
-        # 3. One Hot Encoding
-        if self.features_ohe is not None:
-            self.df = self._get_features()
-
-        return self.df
-
     def get_profiling_df(self):
         # number of products per customer
-        df_stockCode = pd.DataFrame(self.df.groupby('CustomerID')['StockCode'].count()).rename(
-            columns={'StockCode': '#_stockCode'})
+        df_stockCode = pd.DataFrame(self.df.groupby('CustomerID')['StockCode'].count()) \
+            .rename(columns={'StockCode': '#_stockCode'})
         # number of invoices per customer
-        df_InvoiceNo = pd.DataFrame(self.df.groupby('CustomerID')['InvoiceNo'].nunique()).rename(
-            columns={'InvoiceNo': '#_InvoiceNo'})
+        df_InvoiceNo = pd.DataFrame(self.df.groupby('CustomerID')['InvoiceNo'].nunique()) \
+            .rename(columns={'InvoiceNo': '#_InvoiceNo'})
         # avg quantity of all products per customer
-        df_Q = pd.DataFrame(self.df.groupby('CustomerID')['Quantity'].mean()).rename(columns={'Quantity': 'avg_Q'})
+        df_Q = pd.DataFrame(self.df.groupby('CustomerID')['Quantity'].mean()) \
+            .rename(columns={'Quantity': 'avg_Q'})
         # avg P of all products per customer
-        df_P = pd.DataFrame(self.df.groupby('CustomerID')['UnitPrice'].mean()).rename(columns={'UnitPrice': 'avg_P'})
+        df_P = pd.DataFrame(self.df.groupby('CustomerID')['UnitPrice'].mean()) \
+            .rename(columns={'UnitPrice': 'avg_P'})
         # avg Revenue of all products per customer
-        df_Revenue = pd.DataFrame(self.df.groupby('CustomerID')['Revenue'].mean()).rename(
-            columns={'Revenue': 'avg_Revenue'})
+        df_Revenue = pd.DataFrame(self.df.groupby('CustomerID')['Revenue'].mean()) \
+            .rename(columns={'Revenue': 'avg_Revenue'})
         # month with highest Revenue per customer (month when customer spent most money)
-        df_RevenuePerMonth = pd.DataFrame(
-            self.df.groupby(['CustomerID', 'InvoiceMonth'])['Revenue'].sum()).reset_index().sort_values(['Revenue'],
-                                                                                                    ascending=False)
+        df_RevenuePerMonth = pd.DataFrame(self.df.groupby(['CustomerID', 'InvoiceMonth'])['Revenue'].sum()) \
+            .reset_index() \
+            .sort_values(['Revenue'], ascending=False)
         df_HighRevenueMonth = pd.DataFrame(
-            df_RevenuePerMonth[['CustomerID', 'InvoiceMonth']].drop_duplicates(subset='CustomerID',
-                                                                               keep='first')).rename(
-            columns={'InvoiceMonth': 'HighRevenueMonth'})
+            df_RevenuePerMonth[['CustomerID', 'InvoiceMonth']].drop_duplicates(subset='CustomerID', keep='first')
+            ).rename(columns={'InvoiceMonth': 'HighRevenueMonth'})
+        df_HighRevenueMonth.index = df_HighRevenueMonth.CustomerID
+        df_HighRevenueMonth.drop(columns='CustomerID', inplace=True)
 
-        self.df = pd.merge(self.df, df_stockCode, how='left', left_on='CustomerID', right_index=True)
-        self.df = pd.merge(self.df, df_InvoiceNo, how='left', left_on='CustomerID', right_index=True)
-        self.df = pd.merge(self.df, df_Q, how='left', left_on='CustomerID', right_index=True)
-        self.df = pd.merge(self.df, df_P, how='left', left_on='CustomerID', right_index=True)
-        self.df = pd.merge(self.df, df_Revenue, how='left', left_on='CustomerID', right_index=True)
-        self.df = pd.merge(self.df, df_HighRevenueMonth, how='left', left_on='CustomerID', right_on='CustomerID')
-        return self.df
+        lst_df_profiles = [df_stockCode, df_InvoiceNo, df_Q, df_P, df_Revenue, df_HighRevenueMonth]
+        df = self.df.copy()
+        for d in lst_df_profiles:
+            df = pd.merge(df, d, how='left', left_on='CustomerID', right_index=True)
+
+        return df
